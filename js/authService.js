44c4ddb578949ac7cf35ae0792974172a3b92adb
Alexiz Cejudo <alexizncd@gmail.com>
2026-02-27 11:26:52 -0700

    Update authService.js

--- js/authService.js
+++ js/authService.js
@@ -1,7 +1,4 @@
-// js/authService.js
-
 import { auth } from "./config.js";
-
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -9,22 +6,18 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Registrar usuario
-export async function register(email, password) {
-  return await createUserWithEmailAndPassword(auth, email, password);
+export function register(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
 }
 
-// Iniciar sesión
-export async function login(email, password) {
-  return await signInWithEmailAndPassword(auth, email, password);
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
 }
 
-// Cerrar sesión
-export async function logout() {
-  return await signOut(auth);
+export function logout() {
+  return signOut(auth);
 }
 
-// Observar estado de autenticación
 export function observeAuth(callback) {
   onAuthStateChanged(auth, callback);
 }
